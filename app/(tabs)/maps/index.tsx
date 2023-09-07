9e163f5210755d56dd22c86d10649e030e890f53
shadreza <shadreza100@gmail.com>
2023-09-07 13:28:36 +0600

    added map integration, direction, custom markers. have to make the images small and have to make the moving item

--- app/(tabs)/maps/index.tsx
+++ app/(tabs)/maps/index.tsx
@@ -1,13 +1,114 @@
+import { useRef } from 'react';
 import { StyleSheet } from 'react-native';
-
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+// import destinationMarker from '../../../assets/images/destination.png';
+// import startingMarker from '../../../assets/images/start.png';
 import { View } from '../../../components/Themed';
 
 const MapTabScreen = () => {
+
+  const GOOGLE_MAPS_API = process.env.EXPO_PUBLIC_BACKEND_LINK ? process.env.EXPO_PUBLIC_BACKEND_LINK : ''
+  
+  const currentMarker = require('../../../assets/images/current-location.png')
+  const destinationMarker = require('../../../assets/images/destination.png')
+  const startingMarker = require('../../../assets/images/start.png')
+
+
+
+  const mapRef = useRef()
+
+  const geoLocationOfDhaka = {
+    latitude: 23.810332,
+    longitude: 90.41251809999994,
+  }
+
+  const fiveGeoMarkers = [
+    {
+      latitude: 23.81426279756788,
+      longitude: 90.40413862620731,
+    },
+    {
+      latitude: 23.808106397946844,
+      longitude: 90.40328323667977,
+    },
+    {
+      latitude: 23.798141522836097,
+      longitude: 90.40168671183021,
+    },
+    {
+      latitude: 23.78979393341548,
+      longitude: 90.40020439868736,
+    },
+    {
+      latitude: 23.77941034563629,
+      longitude: 90.39837976942222,
+    }
+  ]
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
-      {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: geoLocationOfDhaka.latitude,
+          longitude: geoLocationOfDhaka.longitude,
+          latitudeDelta: 0.0622,
+          longitudeDelta: 0.0121,
+        }}
+        ref={mapRef}
+      >
+        <MapViewDirections
+          origin={fiveGeoMarkers[0]}
+          destination={fiveGeoMarkers[4]}
+          apikey={GOOGLE_MAPS_API}
+          strokeWidth={4}
+          strokeColor="#111111"
+          optimizeWaypoints={true}
+          onReady={(result) => {
+            if (mapRef.current) {
+              mapRef.current.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: 20,
+                  bottom: 40,
+                  left: 20,
+                  top: 40
+                }
+              })
+            }
+          }}
+        />
+
+        {fiveGeoMarkers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            // image={
+            //   index === 0 ?
+            //     startingMarker :
+            //     index === fiveGeoMarkers.length - 1 ?
+            //       destinationMarker :
+            //       currentMarker
+            // }
+          />
+        ))}
+
+        {/* <Polyline
+          coordinates={fiveGeoMarkers}
+          strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeColors={['#7F0000']}
+          strokeWidth={6}
+        />
+
+        <MapViewDirections
+          origin={fiveGeoMarkers[0]}
+          destination={fiveGeoMarkers[fiveGeoMarkers.length - 1]}
+          apikey='AIzaSyBwdGFBTBW04ZA34_9KmttPiybIs96TMfQ' // insert your API Key here
+          strokeWidth={4}
+          strokeColor="red"
+        /> */}
+
+      </MapView>
     </View>
   );
 }
@@ -17,16 +118,9 @@ export default MapTabScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
